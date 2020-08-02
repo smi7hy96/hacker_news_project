19aadd21dd594fe0d1cb947a008a1ace97dc5846
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-08-02 16:06:51 +0100

    provision update

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -14,6 +14,7 @@ sudo apt-get install git -y
 
 sudo apt install nginx -y
 
+
 sudo apt install python3 -y
 
 sudo apt-get install python3-pip -y
@@ -22,29 +23,23 @@ sudo apt install libpython2.7-stdlib -y
 
 sudo install python3-venv -y
 
+#COPY OVER FILES FOR APP
 sudo cp /vagrant/app.py /home/ubuntu
 sudo cp /vagrant/setup.py /home/ubuntu
 sudo cp /vagrant/start.sh /home/ubuntu
 sudo cp /vagrant/requirements.txt /home/ubuntu
-
+#INSTALL REQUIREMENTS
 pip3 install -r requirements.txt -y
 
 sudo apt-get install gunicorn -y
 
+#REMOVE SYMLINK FOR NGINX
+sudo unlink /etc/nginx/sites-enabled/default
+
+sudo nginx -s reload
+
 sudo systemctl restart nginx
 #
+#GUNICORN RUN COMMAND
 # gunicorn -w 3 /home/ubuntu:app
-#
-# cd /home/ubuntu
-#
-# sudo python3 -m venv venv
-#
-# source venv/bin/activate
-#
-# sudo su
-#
-# apt install python-pip -y
-#
-# pip install Flask -y
-#
-#  sudo apt-get install python3-bs4 -y
+# gunicorn -w 2 -b 127.0.0.1:5000 /home/ubuntu:app
