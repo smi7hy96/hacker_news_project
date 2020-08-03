74e43578ac6b68c3121aa8060c8c9e51628076e3
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-08-03 12:35:36 +0100

    updated readme to inculde installation and running steps

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # Hackerank basic API Example
 
 This project is to show a basic implementation of webscraping and generating JSON api data from live sources.
+
+
+## Step 1: CLONE DIRECTORY
+
+`git clone git@github.com:smi7hy96/hacker_news_project.git`
+
+## Step 2: GO TO DIRECTORY on Local Machine
+
+`Vagrant UP`
+
+## This should be running now so going to the web browser should work
+`http://development.local/hacker-news-top-10`
+
+## TO STOP THE APPLICATION RUNNING
+## Step 1: Go into machine
+
+`vagrant ssh app`
+
+## Step 2: Kill GUNICORN Processes
+
+`pkill gunicorn`
+
+## To rerun the application
+## Step 1: Go into machine (if not already in it)
+
+`vagrant ssh app`
+
+## Step 2: Start gunicorn process
+
+`./../ubuntu/app/start.sh`
--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -11,6 +11,17 @@ sudo apt-get install git -y
 
 sudo apt install nginx -y
 
+# REVERSE PROXY NGINX
+sudo unlink /etc/nginx/sites-enabled/default
+
+sudo cp /home/ubuntu/environment/conf/reverse_proxy.conf /etc/nginx/sites-available
+
+sudo ln -s /etc/nginx/sites-available/reverse_proxy.conf /etc/nginx/sites-enabled/reverse_proxy.conf
+
+sudo systemctl restart nginx
+
+#INSTALL PYTHON
+
 sudo apt install python3 -y
 
 sudo apt-get install python3-pip -y
@@ -18,7 +29,13 @@ sudo apt-get install python3-pip -y
 #INSTALL REQUIREMENTS
 pip3 install -r /home/ubuntu/app/requirements.txt
 
+#INSTALL GUNICORN
 sudo apt-get install gunicorn -y
+
+
+#LAUNCH APP
+# gunicorn --bind 127.0.0.1:5000 --chdir /home/ubuntu/app app --daemon
+./../ubuntu/app/start.sh
 
 # gunicorn -b 192.168.10.100:5000 app
 # #REMOVE SYMLINK FOR NGINX
