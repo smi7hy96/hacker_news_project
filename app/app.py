6d8448d2e148cb181bfa6e02e84a6a08b39e5415
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-08-04 14:19:04 +0100

    created landing page to show a result instead of page not found

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 from src.hackerank_news_data_factory import HackerRankDataFactory as HackerRankDataFactory
 
 app = Flask(__name__)
 
 application = app
+
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 @app.route('/hacker-news-top-10')
 def hacker_news_top_10():
