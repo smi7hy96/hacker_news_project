edc80bce9e7dd810b1261cf6c5f7be5189fd921d
smi7hy96 <62227491+smi7hy96@users.noreply.github.com>
2020-08-05 15:03:41 +0100

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # gunicorn -b $1:$2 app:app
-exec gunicorn --chdir /home/ubuntu/app --reload app \
+exec gunicorn --chdir /home/ubuntu/hacker_news_project/app --reload app \
   --bind 127.0.0.1:5000 --daemon
